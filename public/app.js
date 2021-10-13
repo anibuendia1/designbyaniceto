5b3359261f7801e9b8d5b42383a8e58f7099a72a
Aniceto <anibuendia3@gmail.com>
2021-10-13 12:44:48 +0200

    Img try in images folder

--- public/app.js
+++ public/app.js
@@ -2,6 +2,5 @@ const boton = document.querySelector('#boton');
 const menu = document.querySelector('#menu');
 
 boton.addEventListener('click', () => {
-    console.log('click')
     menu.classList.toggle('hidden')
 })
